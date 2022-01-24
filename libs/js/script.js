c6c5e64b12c9d339de71574c80d548d89dcae2a7
ulrichWessmann <97686921+ulrichWessmann@users.noreply.github.com>
2022-01-24 18:05:50 +0000

    capital lat long look up

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,6 +1,6 @@
 // create map
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 2);
         var  Esri_WorldStreetMap = 
             L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
@@ -65,14 +65,27 @@ $(function(){
 				country: $('#countrySelection').val()
 			},
 			success: function(result) {
-                console.log(result.data)
-
-				console.log(JSON.stringify(result));
+                console.log(result)
 
 				if (result.status.name == "ok") {
-
-					console.log(result.data)
-
+                    countries = result.data
+                    index = 0;
+                    if($(".leaflet-interactive")){
+                        $(".leaflet-interactive").remove();
+                    }
+                    for(let i=0; i < countries.features.length; i++){
+                        if(countries.features[i].properties.iso_a2 === $('#countrySelection').val()) {
+                            index = i;
+                        }
+                    }
+                    
+                    L.geoJSON(countries.features[index], {
+                        style:  {
+                            "color": "#ff7800",
+                            "weight": 5,
+                            "opacity": 0.65
+                        }
+                    }).addTo(map);
 				}
 			
 			},
@@ -83,4 +96,59 @@ $(function(){
         
     });
 
-})
+    $("#countrySelection").on("change", ()=> {
+        $.ajax({
+			url: "libs/php/getOpenCageInfo.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				country: $('#countrySelection').val(),
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					
+
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		}); 
+    });
+
+}) // end of document function
+
+
+// AJAX template
+// $.ajax({
+//     url: "libs/php/getCountryInfo.php",
+//     type: 'POST',
+//     dataType: 'json',
+//     data: {
+//         country: $('#selCountry').val(),
+//         lang: $('#selLanguage').val()
+//     },
+//     success: function(result) {
+
+//         console.log(JSON.stringify(result));
+
+//         if (result.status.name == "ok") {
+
+//             $('#txtContinent').html(result['data'][0]['continent']);
+//             $('#txtCapital').html(result['data'][0]['capital']);
+//             $('#txtLanguages').html(result['data'][0]['languages']);
+//             $('#txtPopulation').html(result['data'][0]['population']);
+//             $('#txtArea').html(result['data'][0]['areaInSqKm']);
+
+//         }
+    
+//     },
+//     error: function(jqXHR, textStatus, errorThrown) {
+//         // your error code
+//     }
+// }); 
--- libs/php/getOpenCageInfo.php
+++ libs/php/getOpenCageInfo.php
@@ -1,20 +1,19 @@
 <?php
 
-	// remove for production
-
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+	$key = "6f1183266d26427bb604af7c2266bc31";
 
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
+	$code = $_REQUEST["country"];
+
+	$url='https://restcountries.com/v3.1/alpha/'.$code;
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$url);
 
-	$result=curl_exec($ch);
+	$result = curl_exec($ch);
 
 	curl_close($ch);
 
@@ -24,7 +23,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
